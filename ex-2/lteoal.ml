65bf461a0c648d3a2e5554eee55c57a89acc6168
black-scythe0 <scytheblack991@gmail.com>
2025-07-31 21:47:43 +0530

    another way with tuples

--- ex-2/lteoal.ml
+++ ex-2/lteoal.ml
@@ -1,5 +1,14 @@
+(*lteoal - last two elment of a list*)
 
+(*way-1 with list*) 
 let rec last_two_element = function
-| [] | [_] -> None
-| [x;y] -> Some (x;y)
-| _::l -> last_two_element l;;
+  | [] | [_] -> None
+  | [x;y] -> Some [x;y]
+  | _::l -> last_two_element l;;
+
+(*way-2 with tuples*)
+let rec lte(xy :'a list): ('a * 'a) option = 
+  match xy with 
+  | [] | [_] -> None
+  | [x;y] -> Some(x,y)
+  | _::rest -> lte rest;; 
